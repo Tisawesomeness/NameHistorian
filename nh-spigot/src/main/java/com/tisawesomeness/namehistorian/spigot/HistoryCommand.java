80a4ad1f59953aba03313035c0366baaa8b6e2b1
Tisawesomeness <tis_dev@protonmail.com>
2022-09-10 23:36:57 -0400

    Online players should display current time as last seen

--- nh-spigot/src/main/java/com/tisawesomeness/namehistorian/spigot/HistoryCommand.java
+++ nh-spigot/src/main/java/com/tisawesomeness/namehistorian/spigot/HistoryCommand.java
@@ -42,7 +42,8 @@ public class HistoryCommand implements CommandExecutor, TabCompleter {
         }
         OfflinePlayer player = playerOpt.get();
         try {
-            printNameHistory(sender, plugin.getHistorian().getNameHistory(player.getUniqueId()));
+            List<NameRecord> history = plugin.getHistorian().getNameHistory(player.getUniqueId());
+            printNameHistory(sender, history, player.isOnline());
         } catch (SQLException ex) {
             ex.printStackTrace();
             plugin.sendMessage(sender, ChatColor.RED + "An error occurred while fetching the name history.");
@@ -50,20 +51,29 @@ public class HistoryCommand implements CommandExecutor, TabCompleter {
         return true;
     }
 
-    private void printNameHistory(CommandSender sender, List<NameRecord> nameHistory) {
+    private void printNameHistory(CommandSender sender, List<NameRecord> nameHistory, boolean isOnline) {
         if (nameHistory.isEmpty()) {
             plugin.sendMessage(sender, ChatColor.RED + "No name history found.");
             return;
         }
+
         UUID uuid = nameHistory.get(0).getUuid();
         plugin.sendMessage(sender, "%sName history for %s%s", ChatColor.GOLD, ChatColor.GREEN, uuid);
+
         for (int i = 0; i < nameHistory.size(); i++) {
             NameRecord nr = nameHistory.get(i);
+
+            // History is newest to oldest, changes printed in reverse
             int changeNumber = nameHistory.size() - i;
-            plugin.sendMessage(sender, "%s%d. %s%s", ChatColor.BLUE, changeNumber, ChatColor.LIGHT_PURPLE, nr.getUsername());
-            plugin.sendMessage(sender, "%sFrom: %s%s, %sTo: %s%s",
+            plugin.sendMessage(sender, "%s%d. %s%s",
+                    ChatColor.BLUE, changeNumber,
+                    ChatColor.LIGHT_PURPLE, nr.getUsername());
+
+            // If the player is online, the player was last seen now
+            Instant lastSeen = i == 0 && isOnline ? Instant.now() : nr.getLastSeenTime();
+            plugin.sendMessage(sender, "%sFrom: %s%s %sTo: %s%s",
                     ChatColor.BLUE, ChatColor.GREEN, format(nr.getFirstSeenTime()),
-                    ChatColor.BLUE, ChatColor.GREEN, format(nr.getLastSeenTime())
+                    ChatColor.BLUE, ChatColor.GREEN, format(lastSeen)
             );
         }
     }
